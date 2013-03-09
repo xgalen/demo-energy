fa8a958775c062d2abfd090b54950b91e821eee0
Pedro Narciso García Revington <p.revington@gmail.com>
2013-03-09 18:55:11 +0100

    compression

--- app.js
+++ app.js
@@ -14,10 +14,15 @@ app.configure(function () {
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
+    app.use(express.compress());
     app.use(app.router);
     app.use(express['static'](path.join(__dirname, 'public')));
 });
 app.configure('development', function () {
-    app.use(express.errorHandler());
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
+    app.locals.pretty = true;
 });
 app.get('/', routes.index);
